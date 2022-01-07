bbbcc1324668b04c2350dbd07ce7e2e3aa935fd5
danuapratama <danuaguspratama98@gmail.com>
2022-01-07 19:37:09 +0700

    add color, shadow

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,23 @@
+/*===== SCROLL SECTIONS ACTIVE LINK =====*/
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav-list a[href*=' + sectionId + ']').classList.add('active')
+        }else{
+            document.querySelector('.nav-list a[href*=' + sectionId + ']').classList.remove('active')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
 /*===== CHANGE BACKGROUND NAVBAR =====*/
 function scrollHeader(){
   const header = document.getElementById('navbar')
@@ -77,23 +97,3 @@ Array.from(categories).forEach((item, index) => {
         portfolioMenuList.classList ='portfolio-card '+ e.target.getAttribute('data-portfolio-type')
     }
 })
-
-/*===== SCROLL SECTIONS ACTIVE LINK =====*/
-const sections = document.querySelectorAll('section[id]')
-
-function scrollActive(){
-    const scrollY = window.pageYOffset
-
-    sections.forEach(current =>{
-        const sectionHeight = current.offsetHeight
-        const sectionTop = current.offsetTop - 50;
-        sectionId = current.getAttribute('id')
-
-        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-            document.querySelector('.nav-list a[href*=' + sectionId + ']').classList.add('active')
-        }else{
-            document.querySelector('.nav-list a[href*=' + sectionId + ']').classList.remove('active')
-        }
-    })
-}
-window.addEventListener('scroll', scrollActive)
