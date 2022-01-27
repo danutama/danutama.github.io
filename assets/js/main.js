12f640ac611f8f7c97fbef1745e056418392f95f
danuapratama <danuaguspratama98@gmail.com>
2022-01-27 20:28:46 +0700

    tes accordion section

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,30 +1,31 @@
 /*===== SCROLL SECTIONS ACTIVE LINK =====*/
-const sections = document.querySelectorAll('section[id]')
+const sections = document.querySelectorAll('section[id]');
 
-function scrollActive(){
-    const scrollY = window.pageYOffset
+function scrollActive() {
+  const scrollY = window.pageYOffset;
 
-    sections.forEach(current =>{
-        const sectionHeight = current.offsetHeight
-        const sectionTop = current.offsetTop - 50;
-        sectionId = current.getAttribute('id')
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    sectionId = current.getAttribute('id');
 
-        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-            document.querySelector('.nav-list a[href*=' + sectionId + ']').classList.add('active')
-        }else{
-            document.querySelector('.nav-list a[href*=' + sectionId + ']').classList.remove('active')
-        }
-    })
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document.querySelector('.nav-list a[href*=' + sectionId + ']').classList.add('active');
+    } else {
+      document.querySelector('.nav-list a[href*=' + sectionId + ']').classList.remove('active');
+    }
+  });
 }
-window.addEventListener('scroll', scrollActive)
+window.addEventListener('scroll', scrollActive);
 
 /*===== CHANGE BACKGROUND NAVBAR =====*/
-function scrollHeader(){
-  const header = document.getElementById('navbar')
+function scrollHeader() {
+  const header = document.getElementById('navbar');
   // When the scroll is greater than 10 viewport height, add the scroll-header class to the navbar tag
-  if(this.scrollY >= 10) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+  if (this.scrollY >= 10) header.classList.add('scroll-header');
+  else header.classList.remove('scroll-header');
 }
-window.addEventListener('scroll', scrollHeader)
+window.addEventListener('scroll', scrollHeader);
 
 /*===== SHOW MODAL =====*/
 const showModal = (openButton, modalContent, body) => {
@@ -34,8 +35,7 @@ const showModal = (openButton, modalContent, body) => {
 
   if (openBtn && modalContainer) {
     openBtn.addEventListener('click', () => {
-      modalContainer.classList.add('show-modal'),
-      body_Container.classList.add('show-modal');
+      modalContainer.classList.add('show-modal'), body_Container.classList.add('show-modal');
     });
   }
 };
@@ -46,9 +46,8 @@ const closeBtn = document.querySelectorAll('.close-modal');
 
 function closeModal() {
   const modalContainer = document.getElementById('modal-container'),
-  body_Container = document.getElementById('body');
-  modalContainer.classList.remove('show-modal'),
-  body_Container.classList.remove('show-modal');
+    body_Container = document.getElementById('body');
+  modalContainer.classList.remove('show-modal'), body_Container.classList.remove('show-modal');
 }
 closeBtn.forEach((c) => c.addEventListener('click', closeModal));
 
@@ -83,27 +82,27 @@ themeButton.addEventListener('click', () => {
 });
 
 // === Portfolio Category === //
-let portfolioMenuList = document.querySelector('.portfolio-card')
+let portfolioMenuList = document.querySelector('.portfolio-card');
 
-let portfolioCategory = document.querySelector('.portfolio-category')
+let portfolioCategory = document.querySelector('.portfolio-category');
 
-let categories = portfolioCategory.querySelectorAll('button')
+let categories = portfolioCategory.querySelectorAll('button');
 
 Array.from(categories).forEach((item, index) => {
-    item.onclick = (e) => {
-        let currCat = portfolioCategory.querySelector('button.active')
-        currCat.classList.remove('active')
-        e.target.classList.add('active')
-        portfolioMenuList.classList ='portfolio-card '+ e.target.getAttribute('data-portfolio-type')
-    }
-})
- 
+  item.onclick = (e) => {
+    let currCat = portfolioCategory.querySelector('button.active');
+    currCat.classList.remove('active');
+    e.target.classList.add('active');
+    portfolioMenuList.classList = 'portfolio-card ' + e.target.getAttribute('data-portfolio-type');
+  };
+});
+
 // === Initialize Swiper Sponsor === //
-let swiper = new Swiper(".sponsor__container", {
+let swiper = new Swiper('.sponsor__container', {
   grabCursor: true,
   loop: true,
   centeredSlides: true,
-  slidesPerView: "3",
+  slidesPerView: '3',
   spaceBetween: 130,
   autoplay: {
     delay: 2000,
@@ -114,4 +113,18 @@ let swiper = new Swiper(".sponsor__container", {
     dynamicBullets: true,
     clickable: true,
   },
-})
+});
+
+// === ACCORDION === //
+let li = document.querySelectorAll('.faq-text li');
+for (var i = 0; i < li.length; i++) {
+  li[i].addEventListener('click', (e) => {
+    let clickedLi;
+    if (e.target.classList.contains('question-arrow')) {
+      clickedLi = e.target.parentElement;
+    } else {
+      clickedLi = e.target.parentElement.parentElement;
+    }
+    clickedLi.classList.toggle('showAnswer');
+  });
+}
