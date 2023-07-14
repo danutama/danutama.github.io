2ee53187770b2c23d0b1f0013b037cb90a9a671d
danuapratama <danuaguspratama98@gmail.com>
2023-07-14 23:46:15 +0700

    update color and GSAP animation on Navbar

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,16 +15,39 @@ $(window).on('load', function () {
 //*========== NAVBAR TOGGLE ==========*//
 const navbarItems = document.querySelector('.navbar-items');
 const hamburger = document.querySelector('.hamburger');
+const mediaQuery = window.matchMedia('(max-width: 500px)');
 
 let t1 = gsap.timeline({ paused: true });
 // t1.to(".navbar-items", { opacity: 1, duration: 1, top: 0 });
 t1.fromTo(".navbar-items", { top: "-1000%" }, { top: "0", duration: .8, ease: "cubic-bezier(0.68, -0.55, 0.27, 1.55)" });
-t1.to(".navbar-link", { opacity: 1, duration: 1, stagger: 0.2, ease: "ease-out" });
+
+if (mediaQuery.matches) {
+  t1.fromTo(".navbar-link", { y: "50%"}, { y: "0", opacity: 1, duration: 0.3, stagger: 0.2, ease: "Expo.easeInOut" });
+  t1.fromTo(".navbar-link-credit", { y: "50%"}, { y: "0", opacity: 1, duration: 0.3, stagger: 0.2, ease: "Expo.easeInOut" });
+}
 
 hamburger.addEventListener('change', () => {
   navbarItems.classList.toggle('active');
   t1.restart().timeScale(1);
 });
+
+// SCROLL HIDE AND SHOW
+const navbar = document.getElementById('nav')
+let previousScrollPosition = window.pageYOffset
+
+window.addEventListener('scroll', () => {
+  const currentScrollPosition = window.pageYOffset
+
+  if (!navbarItems.classList.contains('active')) {
+    if (currentScrollPosition < previousScrollPosition) {
+      gsap.to(navbar, { top: '0', duration: 0.3 })
+    } else {
+      gsap.to(navbar, { top: '-100px', duration: 0.3 })
+    }
+  }
+
+  previousScrollPosition = currentScrollPosition
+})
 //*========== END ==========*//
 
 /*========== SCROLL ACTIVE SECTIONS ==========*/
@@ -109,7 +132,7 @@ function updateTime() {
   }
 
   document.getElementById("time").innerHTML =
-    "Local time / " + day + " " + months[monthIndex] + " - " + hours + ":" + minutes + ":" + seconds + " WIB";
+    "Local time: " + day + " " + months[monthIndex] + ", " + hours + ":" + minutes + ":" + seconds + " WIB";
 }
 
 setInterval(function() {
