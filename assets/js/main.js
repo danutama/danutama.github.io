0a89145c372229780980e63a7673630ec95ca700
danuapratama <danuaguspratama98@gmail.com>
2023-07-17 00:44:51 +0700

    fix GSAP Reserve on navbar

--- assets/js/main.js
+++ assets/js/main.js
@@ -62,16 +62,21 @@ const mediaQuery = window.matchMedia('(max-width: 500px)');
 
 let t1 = gsap.timeline({ paused: true });
 // t1.to(".navbar-items", { opacity: 1, duration: 1, top: 0 });
-t1.fromTo(".navbar-items", { top: "-1000%" }, { top: "0", duration: 1, ease: "bounce" });
+t1.fromTo(".navbar-items", { top: "-1000%" }, { top: "0", duration: 1, ease: "expo" });
 
 if (mediaQuery.matches) {
-  t1.fromTo(".navbar-link", { y: "50%"}, { y: "0", opacity: 1, duration: 0.3, stagger: 0.1, ease: "Expo.easeInOut" });
+  t1.fromTo(".navbar-link", { y: "50%"}, { y: "0", opacity: 1, duration: 0.3, stagger: 0.2, ease: "Expo.easeInOut" });
   t1.fromTo(".navbar-link-credit", { y: "50%"}, { y: "0", opacity: 1, duration: 0.3, stagger: 0.2, ease: "Expo.easeInOut" });
 }
 
 hamburger.addEventListener('change', () => {
   navbarItems.classList.toggle('active');
-  t1.restart().timeScale(1);
+  
+  if (navbarItems.classList.contains('active')) {
+    t1.play();
+  } else {
+    t1.reverse();
+  }
 });
 
 
