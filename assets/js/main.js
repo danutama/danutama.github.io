69eca4c5b03ff5565380b7b80096705fbdd9bd20
danuapratama <danuaguspratama98@gmail.com>
2023-07-16 18:38:07 +0700

    add gsap animation for hero-name

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,10 +5,23 @@ $(window).on('hashchange', function (e) {
 //*========== END ==========*//
 
 //*========== LOADING SCREEN ==========*//
-$(window).on('load', function () {
-  setTimeout(function () {
-    $('.loading-screen').fadeOut(200);
-  }, 3000); // 3
+// $(window).on('load', function () {
+//   setTimeout(function () {
+//     $('.loading-screen').fadeOut(200);
+//   }, 3000); // 3
+// });
+
+$(window).on('load', function() {
+  setTimeout(function() {
+    $('.loading-screen').fadeOut(200, function() {
+      gsap.to('.hero-name', {
+        opacity: 1,
+        y: 0,
+        duration: .8,
+        ease: 'bounce',
+      });
+    });
+  }, 3000);
 });
 //*========== END ==========*//
 
@@ -49,10 +62,10 @@ const mediaQuery = window.matchMedia('(max-width: 500px)');
 
 let t1 = gsap.timeline({ paused: true });
 // t1.to(".navbar-items", { opacity: 1, duration: 1, top: 0 });
-t1.fromTo(".navbar-items", { top: "-1000%" }, { top: "0", duration: .8, ease: "cubic-bezier(0.68, -0.55, 0.27, 1.55)" });
+t1.fromTo(".navbar-items", { top: "-1000%" }, { top: "0", duration: 1, ease: "bounce" });
 
 if (mediaQuery.matches) {
-  t1.fromTo(".navbar-link", { y: "50%"}, { y: "0", opacity: 1, duration: 0.3, stagger: 0.2, ease: "Expo.easeInOut" });
+  t1.fromTo(".navbar-link", { y: "50%"}, { y: "0", opacity: 1, duration: 0.3, stagger: 0.1, ease: "Expo.easeInOut" });
   t1.fromTo(".navbar-link-credit", { y: "50%"}, { y: "0", opacity: 1, duration: 0.3, stagger: 0.2, ease: "Expo.easeInOut" });
 }
 
