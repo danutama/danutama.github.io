7079a794748ad3d46dfda5270a3630f7b81b3a03
danuapratama <danuaguspratama98@gmail.com>
2023-07-17 12:39:50 +0700

    fix GSAP animation on navbar

--- assets/js/main.js
+++ assets/js/main.js
@@ -55,33 +55,57 @@ letters.forEach((letter, index) => {
 });
 //*========== END ==========*//
 
-//*========== NAVBAR TOGGLE ==========*//
+//*========== NAVBAR TOGGLE WITH GSAP ANIMATION ==========*//
 const navbarItems = document.querySelector('.navbar-items');
 const hamburger = document.querySelector('.hamburger');
 const mediaQuery = window.matchMedia('(max-width: 500px)');
 
-let t1 = gsap.timeline({ paused: true });
-// t1.to(".navbar-items", { opacity: 1, duration: 1, top: 0 });
-t1.fromTo(".navbar-items", { top: "-1000%" }, { top: "0", duration: 1, ease: "expo" });
+let t1;
 
-if (mediaQuery.matches) {
-  t1.fromTo(".navbar-link", { y: "50%"}, { y: "0", opacity: 1, duration: 0.3, stagger: 0.2, ease: "Expo.easeInOut" });
-  t1.fromTo(".navbar-link-credit", { y: "50%"}, { y: "0", opacity: 1, duration: 0.3, stagger: 0.2, ease: "Expo.easeInOut" });
-}
+const initTimelineAnimation = () => {
+  t1 = gsap.timeline({ paused: true });
+  t1.fromTo(".navbar-items", { top: "-1000%" }, { top: "0", duration: 1, ease: "expo.inOut" });
+  t1.fromTo(".navbar-link", { y: "50%", opacity: 0 }, { y: "0", opacity: 1, duration: 0.3, stagger: 0.2, ease: "Expo.easeInOut" });
+  t1.fromTo(".navbar-link-credit", { y: "50%" }, { y: "0", opacity: 1, duration: 0.3, stagger: 0.2, ease: "Expo.easeInOut" });
+};
 
-hamburger.addEventListener('change', () => {
+// Function to handle navbar link visibility and animation
+const handleNavbarLink = () => {
+  if (mediaQuery.matches) {
+    gsap.set(".navbar-link", { y: "50%", opacity: 0 });
+    gsap.set(".navbar-link-credit", { y: "50%", opacity: 0 });
+  } else {
+    gsap.set(".navbar-link", { y: "0", opacity: 1 });
+    gsap.set(".navbar-link-credit", { y: "0", opacity: 1 });
+  }
+};
+
+// Function to handle menu open/close animation
+const handleMenuAnimation = () => {
   navbarItems.classList.toggle('active');
-  
+
   if (navbarItems.classList.contains('active')) {
     t1.play().timeScale(1);
   } else {
     t1.timeScale(2);
     t1.reverse();
   }
+};
+
+// Initialize the timeline animation
+initTimelineAnimation();
+
+// Check navbar link visibility and animation on page load
+handleNavbarLink();
+
+hamburger.addEventListener('change', handleMenuAnimation);
+
+window.addEventListener('resize', () => {
+  initTimelineAnimation();
+  handleNavbarLink();
 });
 
-
-// SCROLL HIDE AND SHOW
+// HIDE AND SHOW NAVBAR WHEN SCROLLING
 const navbar = document.getElementById('nav')
 const navbar_Items = document.querySelector('.navbar-items');
 let previousScrollPosition = window.pageYOffset;
